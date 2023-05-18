5419cbbaefb716b949a06f891ac51bace8aff9b1
Engr-zainjaved <2017ch272@student.uet.edu.pk>
2023-05-18 21:14:59 +0500

    adding display face box

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Clarifai from 'clarifai';
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import React, { Component } from 'react';
 
-const returnClarifyRequestOptions = (imageUrl) => {
-  const PAT = '6262ca53800e4e95b7dd41fe38be0799';
-  const USER_ID = 'zainjaved742';
-  const APP_ID = 'SmartBrains';
-  const MODEL_ID = 'face-detection';
-  const IMAGE_URL = imageUrl;
-
-  const raw = JSON.stringify({
-    "user_app_id": {
-      "user_id": USER_ID,
-      "app_id": APP_ID
-    },
-    "inputs": [
-      {
-        "data": {
-          "image": {
-            "url": IMAGE_URL
-          }
-        }
-      }
-    ]
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Key ' + PAT
-    },
-    body: raw
-  };
-  return requestOptions;
-}
+const app = new Clarifai.App({
+  apiKey: '578125a5726c40acb1af4f11e45271ea'
+});
 
 class App extends Component {
   constructor() {
@@ -46,39 +17,39 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
     }
   }
 
-  calculateFaceLocation = (data)=>{
-
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col*width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
+
+displayFaceBox = (box)=>{
+  this.setState({box: box});
+}
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-
-    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifyRequestOptions(this.state.input))
-      .then(response => response.json())
-      .then(response => {
-        console.log('hi', response)
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
-            })
-        }
-      })
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -87,12 +58,14 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition  imageUrl={this.state.imageUrl}/>
       </div>
     )
 
   }
 }
-
 export default App;
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ({imageUrl}) => {
     return (
         <div className="flex justify-center h-screen">
             <div className="w-1/2 h-1/2 flex justify-center">
-                <img src={imageUrl} alt="" className="object-contain h-3/4" />
+                <img id="inputimage" src={imageUrl} alt="" className="object-contain h-3/4" />
             </div>
         </div>
     )
